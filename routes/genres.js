609adc239642e9138319841a6e7d376585ef27af
Roy-Gal-Git <Self.roygal@gmail.com>
2021-11-10 13:19:12 +0200

    add middleware function that takes care of user authorization

--- routes/genres.js
+++ routes/genres.js
@@ -30,7 +30,7 @@ router.get('/:id', async (req, res) => {
 
 
 // POST Request -> add genre
-router.post('/add_genre', async (req, res) => {
+router.post('/add_genre', async (req, res) => {  
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message) // 400
   let genre = new Genre({
