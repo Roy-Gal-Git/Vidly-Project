62ca39c124d5124ec2eaec944d199ff245ea5054
Roy-Gal-Git <Self.roygal@gmail.com>
2021-11-11 20:32:23 +0200

    add exception handling

--- index.js
+++ index.js
@@ -15,6 +15,18 @@ require('winston-mongodb');
 const mongoose = require('mongoose');
 app = express();
 
+process.on('uncaughtException', (ex) => {
+  winston.error(ex.message, ex);
+  process.exit(1);
+});
+
+winston.handleExceptions(new winston.transport.File({ filename: 'uncaughtException.log' }))
+
+process.on('unhandledRejection', (ex) => {
+  winston.error(ex.message, ex);
+  process.exit(1);
+});
+
 winston.add(winston.transports.File, { filename: 'logfile.log' })
 winston.add(winston.transports.MongoDB, { db: 'mongodb://localhost/vidly' })
 
