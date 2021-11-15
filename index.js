ed6ed2df0218aa91bfcdea4c20eace160c36a4a2
Roy-Gal-Git <Self.roygal@gmail.com>
2021-11-15 10:41:54 +0200

    Seperate concerns in index.js

--- index.js
+++ index.js
@@ -1,48 +1,12 @@
-const express = require('express');
-const Joi = require('joi');
-const config = require('config');
 const winston = require('winston');
-Joi.objectId = require('joi-objectid')(Joi);
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
-const error = require('./middleware/error');
-require('express-async-errors');
-require('winston-mongodb');
-const mongoose = require('mongoose');
+const express = require('express');
 app = express();
 
-winston.handleExceptions(new winston.transport.File({ filename: 'uncaughtException.log' }))
-
-process.on('unhandledRejection', (ex) => {
-  throw ex
-});
-
-winston.add(winston.transports.File, { filename: 'logfile.log' })
-winston.add(winston.transports.MongoDB, { db: 'mongodb://localhost/vidly' })
-
-if (!config.get('jwtPrivateKey')) {
-  console.log('FATAL ERROR: jwtPrivateKey is not defined.');
-  process.exit(1);
-}
-
-mongoose.connect('mongodb://localhost:27017/vidly')
-  .then(() => console.log('Connected to MongoDB...'))
-  .catch((err) => console.log('Could not connect to MongoDB...', err.message));
-
-
-app.use(express.json());
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-
-app.use(error);
+require('./startup/logging.js')();
+require('./startup/routes')(app);
+require('./startup/db')();
+require('./startup/config')();
+require('./startup/validation')();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {console.log(`Listening on port ${port}...`);});
+app.listen(port, () => { winston.info(`Listening on port ${port}...`); });
---
+++ startup/logging.js
@@ -0,0 +1,14 @@
+const winston = require('winston');
+require('winston-mongodb');
+require('express-async-errors');
+
+module.exports = function() {
+  winston.handleExceptions(new winston.transport.File({ filename: 'uncaughtException.log' }));
+
+  process.on('unhandledRejection', (ex) => {
+    throw ex
+  });
+
+  winston.add(winston.transports.File, { filename: 'logfile.log' });
+  winston.add(winston.transports.MongoDB, { db: 'mongodb://localhost/vidly' });
+};
