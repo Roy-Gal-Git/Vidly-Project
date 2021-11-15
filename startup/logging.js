0dc27ed8afc665897a03cd5021062d5370fec709
Roy-Gal-Git <Self.roygal@gmail.com>
2021-11-15 10:44:56 +0200

    Add console display of logged errors

--- startup/logging.js
+++ startup/logging.js
@@ -3,7 +3,9 @@ require('winston-mongodb');
 require('express-async-errors');
 
 module.exports = function() {
-  winston.handleExceptions(new winston.transport.File({ filename: 'uncaughtException.log' }));
+  winston.handleExceptions(
+    new winston.transport.Console({ colorize: true, prettyPrint: true }),
+    new winston.transport.File({ filename: 'uncaughtException.log' }));
 
   process.on('unhandledRejection', (ex) => {
     throw ex
