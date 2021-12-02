8bfd59ebcd070ceeb5108d721ca50488e013fc2c
Roy-Gal-Git <Self.roygal@gmail.com>
2021-12-02 02:08:33 +0200

    Quick fix remove lines

--- startup/logging.js
+++ startup/logging.js
@@ -8,15 +8,8 @@ module.exports = function() {
     new winston.transports.File({ filename: 'uncaughtExceptions.log' }));
 
   process.on('unhandledRejection', (ex) => {
-    process.exit(1);
     throw ex
   });
-  process.on('SIGTERM', (signal) => {
-    process.exit(1);
-  });
-  process.on('SIGINT', (signal) => {
-    process.exit(1)
-  })
 
   winston.add(winston.transports.File, { filename: 'logfile.log' });
   // winston.add(winston.transports.MongoDB, { db: 'mongodb://localhost/vidly' });
