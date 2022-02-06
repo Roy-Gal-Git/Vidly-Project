50f29213fac232e8d297d62b3be7fe829c1eda5a
Roy-Gal-Git <Self.roygal@gmail.com>
2022-02-06 14:59:48 +0200

    add error handling

--- Frontend/vidly/src/components/loginForm.jsx
+++ Frontend/vidly/src/components/loginForm.jsx
@@ -1,24 +1,60 @@
 import React, { Component } from "react";
-
+import Input from "./common/input";
 class LoginForm extends Component {
+  state = {
+    account: { username: "", password: "" },
+    errors: {},
+  };
+
+  validate = () => {
+    const errors = {};
+    const { account } = this.state;
+
+    if (account.username.trim() === "")
+      errors.username = "Username is required.";
+    if (account.password.trim() === "")
+      errors.password = "Password is required.";
+
+    return Object.keys(errors).length === 0 ? null : errors;
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
+
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log("submitted");
+
+    const errors = this.validate();
+    console.log(errors);
+    this.setState({ errors: errors || {} });
+    if (errors) return;
+
+    console.log("Submitted");
   };
 
   render() {
+    const { account, errors } = this.state;
     return (
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
-          <div className="form-group mb-3">
-            <label htmlFor="username">Username</label>
-            <input id="username" type="text" className="form-control" />
-          </div>
-          <div className="form-group mb-3">
-            <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
-          </div>
+          <Input
+            name="username"
+            value={account.username}
+            label="Username"
+            onChange={this.handleChange}
+            error={errors.username}
+          />
+          <Input
+            name="password"
+            value={account.password}
+            label="Password"
+            onChange={this.handleChange}
+            error={errors.password}
+          />
           <button className="btn btn-primary">Login</button>
         </form>
       </div>
